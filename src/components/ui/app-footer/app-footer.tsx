d33aebbba4c707605192a10a4f0635667eaaa548
Tania Ryabets <tatianaign20@yandex.ru>
2024-11-15 19:48:43 +0300

    Изменены: 1. UI appfooter (стили и разметка) 2. index (провайдер и браузер роутер добавлены 4. app (компонент футера перенесен вниз))

--- src/components/ui/app-footer/app-footer.tsx
+++ src/components/ui/app-footer/app-footer.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
 import styles from './app-footer.module.css';
+import { Link } from 'react-router-dom';
 export const AppFooterUI: FC = () => 
 <footer className={styles.footer}>
     <div className={styles.footer_content}>
+    <Link to="tel:+99995555555" className={styles.phone}> Тел. +7 (999) 999-99-99</Link>
+    <div className={styles.footer_content_copyright}>
         <div className={styles.copyright}><span className={styles.copyright_c}>C</span></div>
-        <p className={styles.year}>2024 We</p>
+        <span className={styles.year}>2024 Все права защищены</span>
+    </div>
     </div>
 </footer>;
-
-// TODO: подумать, что будем писать рядом со значком копирайта, подправить при необходимости
