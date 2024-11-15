9663a1e78d482d9e1d8da773a7497f9f6ac36f7b
Tania Ryabets <tatianaign20@yandex.ru>
2024-11-15 12:46:43 +0300

    component_ui_2_Header_v01

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,7 +1,3 @@
 import { FC } from 'react';
 import { RegisterUI } from '@ui-pages';
-export const Register: FC = () => {
-  return (
-    <RegisterUI />
-  );
-};
+export const Register: FC = () => <RegisterUI />;
