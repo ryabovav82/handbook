33140b5372bb4c071cc2ee3c5ab9eab083fa3705
Dmitriy Nikolaev <frcndk@gmail.com>
2024-11-17 21:04:52 +0500

    app change route and nav

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -4,13 +4,34 @@ import { AppFooter, AppHeader, AppNavigate } from '@components';
 import { AppMain } from '../app-main';
 import { Route, Routes, useLocation } from 'react-router-dom';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <AppFooter />
-    <AppNavigate />
-    <AppMain />
-  </div>
-);
+const App = () => {
+  const location = useLocation();
+
+  const shouldHideNavigate = ['/faq', '/report-error', '/login'].includes(
+    location.pathname
+  );
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+
+      {!shouldHideNavigate && <AppNavigate />}
+
+      <Routes>
+        <Route path='/' element={<AppMain />} />
+
+        <Route path='/faq' element={<h1>FAQ Page</h1>} />
+        <Route path='/report-error' element={<h1>Report Error Page</h1>} />
+        <Route path='/login' element={<h1>Login Page</h1>} />
+
+        <Route path='/card/:id' element={<h1>Card Page</h1>} />
+
+        <Route path='/other-page' element={<h1>Other Page</h1>} />
+      </Routes>
+
+      <AppFooter />
+    </div>
+  );
+};
 
 export default App;
