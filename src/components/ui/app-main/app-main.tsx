51502a77dedfc8d670b22f1a16c896221fc740da
Tania Ryabets <tatianaign20@yandex.ru>
2024-11-21 12:22:23 +0300

    изменила верстку главной страницы и др. мелкие изм, связанные с гл. страницей UI

--- src/components/ui/app-main/app-main.tsx
+++ src/components/ui/app-main/app-main.tsx
@@ -1,3 +1,20 @@
 import React, { FC } from 'react';
 import styles from './app-main.module.css';
-export const AppMainUI: FC = () => <main>Main</main>;
+// export const AppMainUI: FC = () => <main>Main</main>;
+
+export const AppMainUI: FC = () => {
+  const isSelected = false; //при выбранном пункте меню useSelector(state => state.slice.isSelected) В логику потом
+  const isAuthenticated = true; //useSelector(state => state.auth.isAuthenticated); В логику потом
+
+  return (
+    <div className={styles.main}>
+      {!isSelected && 
+       <div> <img className={styles.main_base_img} src= {require('/public/images/main_3.png')} alt='Create handbook' /></div>
+      }
+      <div className={styles.main_cards}>
+        {isSelected && <div>Карточка с сервера</div>}
+        {isSelected && isAuthenticated && <div>Добавить карточку</div>}
+      </div>
+    </div>
+  );
+};
