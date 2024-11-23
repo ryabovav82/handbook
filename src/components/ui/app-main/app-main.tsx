85ee83eaa0f3383e0327da08f70f7dbf928831c9
Tania Ryabets <tatianaign20@yandex.ru>
2024-11-23 21:41:53 +0300

    Реализовала возможность редактирования в зависимости от авторизации

--- src/components/ui/app-main/app-main.tsx
+++ src/components/ui/app-main/app-main.tsx
@@ -1,6 +1,8 @@
-import React, { FC } from 'react';
+import React, { FC, ChangeEvent } from 'react';
 import styles from './app-main.module.css';
 import { ICardProps } from 'src/components/app-main/app-main';
+import { useState } from 'react';
+import { useDispatch } from 'src/services/store';
 
 // export const AppMainUI: FC = () => <main>Main</main>;
 
@@ -14,8 +16,22 @@ export const AppMainUI: FC<ICardProps> = ({
   const isSelected = true; //при выбранном пункте меню useSelector(state => state.slice.isSelected) В логику потом
   const isAuthenticated = false; //useSelector(state => state.auth.isAuthenticated); В логику потом
 
+  const [editedText, setEditedText] = useState(text);
+  const [editedImage, setEditedImage] = useState(image);
+
+  const handleSave = () => {
+    // логика сохранения
+    console.log('Сохранено');
+  };
+
+  const handleDelete = () => {
+    // логика удаления
+    console.log('Удалено');
+  };
+
   return (
     <div className={styles.main}>
+      //{' '}
       {!isSelected && (
         <div className={styles.main_base}>
           {' '}
@@ -29,58 +45,66 @@ export const AppMainUI: FC<ICardProps> = ({
       <div>
         {isSelected && (
           <div className={styles.main_cards}>
-            {/* Карточки с сервера */}
             <div className={styles.main_cards_card}>
               <div className={styles.main_cards_card_img}>
                 <img
                   className={styles.main_base_card_img}
-                  src={image}
+                  src={editedImage}
                   alt={`card-${id}`}
                 />
+                {isAuthenticated && (
+                  <input
+                    className={styles.main_base_card_img_input}
+                    type='text'
+                    value={editedImage}
+                    onChange={(e) => setEditedImage(e.target.value)}
+                  />
+                )}
               </div>
               <div className={styles.main_base_card_text}>
-                <p className={styles.main_base_card_text_p}>{text}</p>
+                {isAuthenticated ? (
+                  <textarea
+                    className={styles.main_base_card_text_input}
+                    value={editedText}
+                    onChange={(e) => setEditedText(e.target.value)}
+                  />
+                ) : (
+                  <p className={styles.main_base_card_text_p}>{editedText}</p>
+                )}
               </div>
-            </div>
-
-            {/* Тут вывод  потом сделать...   
-            <>
-                {data.map((card: ICardProps) => (
-                <Card key={card.id} {...card} />
-                ))}
-            </> */}
-
-            {/* ПРИМЕР отображения карточки с картинкой, потом удалить: */}
-            <div className={styles.main_cards_card}>
-              <div className={styles.main_cards_card_img}>
-                <img
-                  className={styles.main_base_card_img}
-                  src={require('/public/images/img_test1.png')}
-                  alt='example'
-                />
-              </div>
-              <div className={styles.main_base_card_text}>
-                <p className={styles.main_base_card_text_p}>Описание.</p>
-              </div>
-
-              {/* Стили кнопок, использовать в форме */}
-              {/* <div className={styles.main_base_card_buttons}>
-                <button className={styles.main_base_card_button_save} onClick={() => {}}>
-                  Сохранить
-                </button>
-                <button className={styles.main_base_card_button} onClick={() => {}}>
-                  Удалить
-                </button>
-              </div> */}
-            </div>
-            {/* Карточка добавления */}
-            {isAuthenticated && (
-              <div className={styles.main_cards_add}>
-                <div className={styles.main_cards_add_icon} onClick={() => {}}>
-                  <span className={styles.main_cards_add_icon_plus}>+</span>
+              {isAuthenticated && (
+                <div className={styles.main_base_card_buttons}>
+                  <button
+                    className={styles.main_base_card_button_save}
+                    onClick={handleSave}
+                  >
+                    Сохранить
+                  </button>
+                  <button
+                    className={styles.main_base_card_button}
+                    onClick={handleDelete}
+                  >
+                    Удалить
+                  </button>
                 </div>
-              </div>
-            )}
+              )}
+            </div>
+          </div>
+        )}
+
+        {/* Тут вывод  потом сделать...   
+//             <>
+//                 {data.map((card: ICardProps) => (
+//                 <Card key={card.id} {...card} />
+//                 ))}
+//             </> */}
+
+        {/* Карточка добавления */}
+        {isAuthenticated && (
+          <div className={styles.main_cards_add}>
+            <div className={styles.main_cards_add_icon} onClick={() => {}}>
+              <span className={styles.main_cards_add_icon_plus}>+</span>
+            </div>
           </div>
         )}
       </div>
