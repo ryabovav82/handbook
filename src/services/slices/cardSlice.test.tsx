79d22b349cd27045da70b33cd97bd8332e27e5db
Tania Ryabets <tatianaign20@yandex.ru>
2024-12-20 19:54:00 +0300

    Подправила тест

--- src/services/slices/cardSlice.test.tsx
+++ src/services/slices/cardSlice.test.tsx
@@ -1,8 +1,19 @@
+import { error } from 'console';
 import { cardsSlice } from './cardSlice';
 import { TCardsState } from './cardSlice';
 import { initialState } from './cardSlice';
+import { getCards, delCard, addCard, changeCardText, changeCardImage } from './cardSlice';
 
-const cardSliceReduser = cardsSlice.reducer;
+// jest.mock('../../utils/handbook-api', () => ({
+//   getCardsApi: jest.fn(),
+//   addCardApi: jest.fn(),
+//   delCardApi: jest.fn(),
+//   changeCardTextApi: jest.fn(),
+//   changeCardImageApi: jest.fn()
+// }));   
+// СДЕЛАТЬ тесты для extraReducers
+
+const cardSliceReducer = cardsSlice.reducer;
 
 const testCard = {
   id: 21,
@@ -19,7 +30,7 @@ describe('проверка cardSliceReducer', () => {
   });
 
   it('проверка добавления карточки', () => {
-    const newState = cardSliceReduser(initialState, {
+    const newState = cardSliceReducer(initialState, {
       type: 'cards/addCard',
       payload: testCard
     });
@@ -29,12 +40,12 @@ describe('проверка cardSliceReducer', () => {
 
   it('проверка удаления карточки', () => {
     const actionBefore = cardsSlice.actions.addCard(testCard);
-    state = cardSliceReduser(state, actionBefore);
+    state = cardSliceReducer(state, actionBefore);
     const action = {
       type: 'cards/removeCard',
       payload: testCard.id
     };
-    const newState = cardSliceReduser(state, action);
+    const newState = cardSliceReducer(state, action);
     expect(newState.data).toHaveLength(1);
     expect(newState.data.map((card) => card.id)).not.toContain('testCard.id');
   });
@@ -49,7 +60,7 @@ describe('проверка cardSliceReducer', () => {
       data: [testCard]
     };
 
-    const newState = cardSliceReduser(modifiedInitialState, {
+    const newState = cardSliceReducer(modifiedInitialState, {
       type: 'cards/changeCard',
       payload: testCardEdit
     });
